b023b4357416a36330d1927eaa4d708911f3b4a0
Felix Krause <krausefx@gmail.com>
2021-11-30 18:11:59 +0100

    Finished implementation

---
+++ scheduler.rb
@@ -0,0 +1,22 @@
+require_relative "./database.rb"
+require "telegram/bot"
+require 'date'
+
+if Date.today.month == 12 && Date.today.day < 25
+  day_to_use = Date.today.day
+else
+  exit
+end
+
+::Telegram::Bot::Client.run(ENV.fetch("TELEGRAM_API_TOKEN")) do |bot|
+  Advent::Database.database[:groups].exclude(targetToPostId: nil).each do |group|
+    todays_photo = Advent::Database.database[:photos].where(group_id: group[:id]).order(:id).to_a[day_to_use - 1]
+
+    bot.api.send_message(chat_id: group[:targetToPostId], text: "🕯 #{day_to_use}. Dezember 2021")
+    bot.api.send_photo(
+      chat_id: group[:targetToPostId], 
+      photo: todays_photo[:file_id]
+    )
+  end
+end
+
